2b93df8af964c97c7e65edcdd8b0554c54f8db8c
kmminhaz <kmminhaj.khan@gmail.com>
2022-05-25 01:10:56 +0600

    Telwind install and NavBar Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/HomePage/Home';
+import NavBar from './Pages/Navigation/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the Metal House</h1>
+    <div className='App'>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+      </Routes>
+
     </div>
   );
 }
---
+++ src/Pages/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h5>This is Blogs Page</h5>
+        </div>
+    );
+};
+
+export default Blogs;
